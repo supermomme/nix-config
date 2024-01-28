6515f116479be20792126f8abf2aacb8fd97cf59
Momme Jürgensen <momme@juergensen.me>
2024-01-29 00:08:04 +0100

    add ntfy

--- hosts/spiffy/configuration.nix
+++ hosts/spiffy/configuration.nix
@@ -82,6 +82,19 @@ in
     };
   };
 
+  ### ntfy
+  services.ntfy-sh = {
+    enable = true;
+    settings = {
+      base-url = "https://ntfy.momme.world";
+      listen-http = ":4363";
+      behind-proxy = true;
+      auth-default-access = "write-only";
+    };
+  };
+  services.nginx.virtualHosts."ntfy.momme.world" = (createProxyHost { sslCert = "momme.world"; targetPort = 4363; });
+
+
   # ### syncthing
   # services.syncthing = {
   #   enable = true;
